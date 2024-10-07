edc45e2a2b2507463048ef6f5b6cd698bb3d08e2
Jibril Richter <jibrilrichter@gmail.com>
2024-10-07 13:31:11 -0500

    solution OverrideArrayDuplicates

---
+++ src/main/java/com/jibril/richter/OverrideArrayDublicate.java
@@ -0,0 +1,17 @@
+package com.jibril.richter;
+
+public class OverrideArrayDublicate {
+
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int j = 1;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[i - 1]) {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+            return j;
+        }
+    }
+}
