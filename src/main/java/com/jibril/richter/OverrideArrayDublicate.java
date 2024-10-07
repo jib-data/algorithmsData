0ee65434a2dd837787945d8eac04a6107fdd947a
Jibril Richter <jibrilrichter@gmail.com>
2024-10-07 15:08:28 -0500

    edit

--- src/main/java/com/jibril/richter/OverrideArrayDublicate.java
+++ src/main/java/com/jibril/richter/OverrideArrayDublicate.java
@@ -2,16 +2,14 @@ package com.jibril.richter;
 
 public class OverrideArrayDublicate {
 
-    class Solution {
-        public int removeDuplicates(int[] nums) {
-            int j = 1;
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i] != nums[i - 1]) {
-                    nums[j] = nums[i];
-                    j++;
-                }
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
             }
-            return j;
         }
+        return j;
     }
 }
